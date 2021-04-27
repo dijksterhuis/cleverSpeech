927f94d52de1e092db9ae095e1f9dc4aecc627d7
dijksterhuis <mrobeson@dundee.ac.uk>
2021-04-27 21:44:55 +0100

    ADD: New script to colate all the stats files togther.

--- bin/attacks/analysis/join_stats_csv_files.sh
+++ bin/attacks/analysis/join_stats_csv_files.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+
+EXPERIMENTS_BASE_DIR=${1}
+
+function get_all_stats_files () {
+    find ${1} -type f -name "stats.csv"
+}
+
+head -n 1  $(get_all_stats_files ${EXPERIMENTS_BASE_DIR} | head -n 1) > compiled_stats.csv
+find ${EXPERIMENTS_BASE_DIR} -type f -name "stats.csv" -exec tail -n+2 {} \; >> compiled_stats.csv
