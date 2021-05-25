6ed60284f6db2fb73fe42068062bd9c3ebb4aeaf
dijksterhuis <mrobeson@dundee.ac.uk>
2021-05-25 19:04:35 +0100

    FIX: Silence data set was not being generated correctly. added metadata jsons (with blank true transcriptions) and tidied up the script.

--- bin/mcv/make_aws_archive/make_silence.py
+++ bin/mcv/make_aws_archive/make_silence.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import librosa
 import soundfile
 from random import randint
@@ -20,17 +21,31 @@ def main(outdir):
         length = randint(MIN_LENGTH_EXAMPLES, MAX_LENGTH_EXAMPLES)
 
         silence = np.zeros(length, dtype=np.int16)
-        basename = "silence_{i}.wav".format(i=example_id)
-        file_path = os.path.abspath(
-            os.path.join(outdir, basename)
+        base_file_name = "silence_{i}".format(i=example_id)
+        base_file_path = os.path.abspath(
+            os.path.join(outdir, base_file_name)
         )
+
+        wav_file_name = base_file_path + ".wav"
+        json_file_name = base_file_path + ".json"
+
         soundfile.write(
-            file_path,
+            wav_file_name,
             silence,
             16000,
             subtype="PCM_16",
             format="WAV"
         )
+
+        json_data = [
+            {
+                "correct_transcription": "",
+                "n_samples": length
+            }
+        ]
+
+        with open(json_file_name, "w+") as f:
+            json.dump(json_data, f)
 
 
 if __name__ == '__main__':
