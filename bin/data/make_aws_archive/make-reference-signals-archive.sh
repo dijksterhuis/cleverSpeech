7539402f0dbee8b599675afd7a02060c5e6e8a6e
dijksterhuis <mrobeson@dundee.ac.uk>
2021-06-22 17:08:26 +0100

    UPDATE: Need the "all" directory because of legacy decisions (I should fix this, but oh well).

--- bin/data/make_aws_archive/make-reference-signals-archive.sh
+++ bin/data/make_aws_archive/make-reference-signals-archive.sh
@@ -8,29 +8,36 @@ then
   exit
 fi
 
-mkdir -p ./reference-signals/sines/sine \
-    ./reference-signals/sines/additive \
-    ./reference-signals/sines/am \
-    ./reference-signals/sines/fm \
-    ./reference-signals/noise/uniform
+mkdir -p ./reference-signals/sines/sine/all \
+    ./reference-signals/sines/additive/all \
+    ./reference-signals/sines/am/all \
+    ./reference-signals/sines/fm/all \
+    ./reference-signals/noise/uniform/all
 
 echo "Creating basic sine files."
-python3 ./bin/data/make_aws_archive/reference-signals/make_sine_tones.py ./reference-signals/sines/sine
+python3 ./bin/data/make_aws_archive/reference-signals/make_sine_tones.py ./reference-signals/sines/sine/all
 
 echo "Creating additive sine files."
-python3 ./bin/data/make_aws_archive/reference-signals/make_additive_sine_tones.py ./reference-signals/sines/additive
+python3 ./bin/data/make_aws_archive/reference-signals/make_additive_sine_tones.py ./reference-signals/sines/additive/all
 
 echo "Creating am sine files."
-python3 ./bin/data/make_aws_archive/reference-signals/make_am_sine_tones.py ./reference-signals/sines/am
+python3 ./bin/data/make_aws_archive/reference-signals/make_am_sine_tones.py ./reference-signals/sines/am/all
 
 echo "Creating fm sine files."
-python3 ./bin/data/make_aws_archive/reference-signals/make_fm_sine_tones.py ./reference-signals/sines/fm
+python3 ./bin/data/make_aws_archive/reference-signals/make_fm_sine_tones.py ./reference-signals/sines/fm/all
 
 echo "Creating uniform noise files."
-python3 ./bin/data/make_aws_archive/reference-signals/make_uniform_noise.py ./reference-signals/noise/uniform
+python3 ./bin/data/make_aws_archive/reference-signals/make_uniform_noise.py ./reference-signals/noise/uniform/all
 
-echo "Getting targets file"
-cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/
+echo "Copying targets files"
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/sines/sine
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/sines/additive
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/sines/am
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/sines/fm
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/sines/sine
+cp -fv ${MCV_CORPUS_DIR}/cv-valid-test.csv ./reference-signals/noise/uniform
+
+
 
 # create archive for S3
 echo "Archiving."
