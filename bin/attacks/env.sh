067d9ed179c7918ebbf9bae4513c2dc882289505
dijksterhuis <mrobeson@dundee.ac.uk>
2021-08-11 16:13:49 +0100

    UPDATE: Bumped DS version to 0.9.1 and changed model location to be a part of graph as importing from somewhere else was very hacky and annoying.

--- bin/attacks/env.sh
+++ bin/attacks/env.sh
@@ -2,9 +2,3 @@
 
 echo "Add cleverSpeech to your PYTHONPATH..."
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-echo "Add models to your PYTHONPATH..."
-for model_dir in ./models/*/src/
-do
-    export PYTHONPATH="${PYTHONPATH}:$(pwd)/${model_dir}"
-done
