e88adf29a4569357ae7e85a48c5647a69eccf6ca
dijksterhuis <mrobeson@dundee.ac.uk>
2021-09-07 22:42:57 +0100

    UPDATE: Redid the user switch dance with GNU parallel to save time.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,14 +12,30 @@ if [[ -z ${LOCAL_UID} && -z ${LOCAL_GID} ]]
 then
     echo ""
 else
-    echo "Chowning files... This may take a while."
+    echo "Modifying container for UID=${LOCAL_UID} and GID=${LOCAL_GID}"
+    echo "=> chown-ing files..."
+    N_CPUS=$(cat /proc/cpuinfo | grep -c processor)
+
+    find /home/cleverspeech \
+      -path "*/native_client/*" -prune -o \
+      -path "*/modules/*" -prune -o  \
+      -print \
+    | parallel -j $((${N_CPUS} * 8)) \
+      chown --changes ${LOCAL_UID}:${LOCAL_GID} ::: >> /chown.log 2>&1
+
+    echo "==> switching user and group..."
+
     if [[ -z $(getent group ${LOCAL_GID}) ]]
     then
         groupadd -g ${LOCAL_GID} local
     fi
-    usermod -u ${LOCAL_UID} -g ${LOCAL_GID} cleverspeech
-    chown -R ${LOCAL_UID}:${LOCAL_GID} /home/cleverspeech/
+    usermod -u ${LOCAL_UID} -aG ${LOCAL_GID} cleverspeech
+
+    echo "==> all done."
+
+
 fi
 
 exec sudo -E -H -u cleverspeech ${cmd}
+
 
