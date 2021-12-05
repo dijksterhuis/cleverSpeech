2103090d86dff47578cf51ec23df304fb206c9ce
dijksterhuis <mrobeson@dundee.ac.uk>
2021-12-05 21:41:42 +0000

    uploads and downloads can be simplified

--- bin/data/get-tar-from-aws.sh
+++ bin/data/get-tar-from-aws.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-SAMPLES_URL=https://cleverspeech-data.s3.eu-west-2.amazonaws.com/samples.tar.gz
-SILENCE_URL=https://cleverspeech-data.s3.eu-west-2.amazonaws.com/silence.tar.gz
+ARCHIVE_FILE_NAME=mcv1-sentences.tar.gz
+SAMPLES_URL=https://cleverspeech-data.s3.eu-west-2.amazonaws.com/${ARCHIVE_FILE_NAME}
 
-if [[ -f samples.tar.gz ]]
+if [[ -f ${ARCHIVE_FILE_NAME} ]]
 then
     echo "samples.tar.gz archive exists so skipping download"
 else
     echo "Getting samples archive."
-    curl -o ./samples.tar.gz ${SAMPLES_URL} \
+    curl -o ./${ARCHIVE_FILE_NAME} ${SAMPLES_URL} \
         && echo "Got samples archive."
 fi
 
 # make samples directory and dump files based on ID.
 
 echo "Extracting samples archive."
-tar -xzf ./samples.tar.gz \
+tar -xzf ./${ARCHIVE_FILE_NAME} \
     && echo "Extraction successful."
 
 echo "====>: ./samples/ prep script completed. <===="
---
+++ bin/data/put-tar-to-s3.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo """
+  This script is for minimal organisation on original mozilla common voice test
+  data sets and pushes source data to my own S3 repository. It is not intended
+  for use by others.
+  """
+
+MCV_CORPUS_DIR=${1}
+# e.g. mcv7
+DATASET_MAJOR_ID=${2}
+# eg. sentences
+DATASET_MINOR_ID=${3}
+
+WAV_DIR=${MCV_CORPUS_DIR}
+TRANSCRIPTION_FILE=${MCV_CORPUS_DIR}/test.csv
+NEW_TRANSCRIPTION_DIR=./samples/${DATASET_MAJOR_ID}/${DATASET_MINOR_ID}/
+NEW_WAV_DIR=${NEW_TRANSCRIPTION_DIR}/all/
+ARCHIVE_NAME=${DATASET_MAJOR_ID}-${DATASET_MINOR_ID}.tar.gz
+
+
+if [[ -z ${MCV_CORPUS_DIR} ]]
+then
+  echo "I need the path to the cv_corpus_v1/ directory as an argument please!"
+  exit
+fi
+
+mkdir -p ${NEW_WAV_DIR}
+
+# use find as *.wav globs can cause problems
+echo "Getting audio files."
+find ${WAV_DIR} -type f -name "*.wav" -exec cp -vf {} ${NEW_WAV_DIR} \; \
+
+echo "Getting targets file"
+cp -fv ${TRANSCRIPTION_FILE} ${NEW_TRANSCRIPTION_DIR}/test.csv
+
+echo "Pre-processing audio (normalise, trim) and generating json example files."
+python3 ./bin/data/${DATASET_MAJOR_ID}/preprocess_samples.py \
+  ${NEW_WAV_DIR} ${NEW_TRANSCRIPTION_DIR}/test.csv
+
+# create archive for S3
+echo "Archiving."
+tar cz -f ./${ARCHIVE_NAME} ${NEW_TRANSCRIPTION_DIR} \
+ && rm -rfv ${NEW_TRANSCRIPTION_DIR}
+# upload to s3
+echo "Uploading."
+aws s3 cp ./${ARCHIVE_NAME} s3://cleverspeech-data/${ARCHIVE_NAME} \
+ && echo "Done!"
+
+echo "Cleaning up."
+rm -rfv ./${ARCHIVE_NAME} \
+ && echo "Done!"
