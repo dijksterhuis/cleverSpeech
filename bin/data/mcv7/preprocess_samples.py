183f4302058057b3bd6e6ba2f0575babbb538633
dijksterhuis <mrobeson@dundee.ac.uk>
2021-12-07 14:49:11 +0000

    Do all preprocessing and uploads for mcv7 via python

--- bin/data/mcv7/preprocess_samples.py
+++ bin/data/mcv7/preprocess_samples.py
@@ -3,12 +3,16 @@
 import os
 import sys
 import json
-import time
+import boto3
+import shutil
+import tarfile
 import soundfile
-import progressbar
 
 import numpy as np
 import multiprocessing as mp
+
+from cleverspeech.utils.Utils import Logger
+from progressbar import ProgressBar
 
 BIT_DEPTH = 2 ** 15
 
@@ -17,7 +21,7 @@ def l_map(f, x):
     return list(map(f, x))
 
 
-def load_wav(fp):
+def get_wav_file_size(fp):
     dtype = "int16"
     audio, sample_rate = soundfile.read(fp, dtype=dtype)
     try:
@@ -27,13 +31,13 @@ def load_wav(fp):
         err += "min(audio)={}\n".format(min(audio))
         err += "max(audio)={}\n".format(max(audio))
         err += "input must be valid 16 bit audio: -2**15 <= x <= 2**15 -1\n"
-        print(err)
+        Logger.critical(err)
         raise AssertionError
     return audio
 
 
 def load_wavs(fps, dtype="int16"):
-    return [load_wav(f) for f in fps]
+    return [get_wav_file_size(f) for f in fps]
 
 
 def load_transcriptions(csv_file_path):
@@ -47,9 +51,9 @@ def load_transcriptions(csv_file_path):
         return split_row[0].replace(".wav", '')
 
     def get_sample_transcription(split_row):
-        return split_row[2].rstrip("\n")
+        return split_row[2]
 
-    rows = [row.split(',') for row in rows]
+    rows = [row.rstrip("\n").split(',') for row in rows]
 
     sample_trans_map = {
         get_sample_basename_key(r): get_sample_transcription(r) for r in rows
@@ -58,96 +62,214 @@ def load_transcriptions(csv_file_path):
     return sample_trans_map
 
 
-def main(wav_dir, trans_file_path):
+def make_directory_structure(major_id, minor_id):
+    samples_dir = os.path.join(os.path.join("./samples", major_id), minor_id)
+    os.makedirs(samples_dir, exist_ok=True)
+    return samples_dir
 
-    print(
-        """
-        Note that this script will OVERWRITE your test files with
-        normalised and trimmed versions.\n
-        If you have unintentionally modified your data by running this script 
-        without inspecting it first, then you should pay more attention to 
-        random scripts you downloaded from the internets.
-        """
+
+def move_files(args):
+    initial_path, end_path = args
+    shutil.copy(initial_path, end_path)
+
+
+def get_json_metadata(args) -> list:
+    transcription, wav = args
+    n_samples = wav.size
+
+    return [
+        {
+            "correct_transcription": transcription,
+            "n_samples": int(n_samples),
+        }
+    ]
+
+
+def dump_json(args):
+    path, json_data = args
+    metadata_json_path = path.rstrip(".wav") + ".json"
+    with open(metadata_json_path, "w+") as f:
+        # save disk space in json by not using *any* delimiter
+        # whitespace
+        json.dump(json_data, f, separators=(',', ':'))
+    return metadata_json_path
+
+
+def dump_wav_file(args):
+    path, wav = args
+    soundfile.write(
+        path, wav.astype(np.int16), 16000, subtype="PCM_16", format="WAV"
     )
-    for i in range(10, -1, -1):
-        s = "You have {} seconds until changes are made.".format(i)
-        print(s)
-        time.sleep(1)
+
+
+def process(wav_dir, trans_file_path, samples_dir):
+
+    Logger.info("Loading transcriptions....")
 
     transcription_mapping = load_transcriptions(trans_file_path)
 
-    abs_paths = l_map(
-        lambda fp: os.path.abspath(os.path.join(wav_dir, fp)),
-        os.listdir(wav_dir)
+    initial_transcription_file_path = os.path.join(
+        wav_dir, os.path.basename(trans_file_path)
+    )
+    samples_transcription_file_path = os.path.join(
+        samples_dir, os.path.basename(trans_file_path)
     )
 
-    abs_wav_file_paths = [x for x in abs_paths if not os.path.isdir(x) and ".wav" in x]
+    Logger.info("... Loaded transcriptions.")
+    Logger.info("Moving transcriptions csv file...")
 
-    print("\nHave these wav files (first 10)")
-    for fp in abs_wav_file_paths[0:10]:
+    move_files(
+        (initial_transcription_file_path, samples_transcription_file_path)
+    )
+
+    Logger.info("... Moved transcriptions csv file.")
+    Logger.info("Loading wav file paths...")
+
+    def absolute_file_path(directory, x):
+        return os.path.abspath(os.path.join(directory, x))
+
+    def safety_check(directory, x):
+        return not os.path.isdir(absolute_file_path(directory, x)) and ".wav" in x
+
+    wav_file_paths = [
+        x for x in os.listdir(wav_dir) if safety_check(wav_dir, x)
+    ]
+
+    initial_abs_file_paths = l_map(
+        lambda file_path: absolute_file_path(wav_dir, file_path), wav_file_paths
+    )
+    new_wav_dir = os.path.join(samples_dir, "all")
+    os.makedirs(new_wav_dir, exist_ok=True)
+    samples_abs_file_paths = l_map(
+        lambda file_path: absolute_file_path(new_wav_dir, file_path), wav_file_paths
+    )
+
+    Logger.info("Have these wav files (first 10)")
+    for fp in initial_abs_file_paths[0:10]:
         print(fp)
 
-    print("\nHave these wav files (last 10)")
-    for fp in abs_wav_file_paths[-10:]:
+    Logger.info("Have these wav files (last 10)")
+    for fp in initial_abs_file_paths[-10:]:
         print(fp)
 
-    print("\nLoading wav files...")
+    Logger.info("Moving wav files to {} directory...".format(samples_dir))
     with mp.Pool(mp.cpu_count()) as pool:
-        wavs = pool.map(load_wav, abs_wav_file_paths)
+        pool.map(
+            move_files,
+            zip(initial_abs_file_paths, samples_abs_file_paths)
+        )
+    Logger.info("... wav file move complete")
+    Logger.info("Moving transcription file to {} directory...".format(samples_dir))
 
-    # wavs = load_wavs(abs_wav_file_paths, "int16")
+    with mp.Pool(mp.cpu_count()) as pool:
+        pool.map(
+            move_files,
+            zip(initial_abs_file_paths, samples_abs_file_paths)
+        )
+    Logger.info("... wav file move complete")
+    Logger.info("Loading wav files...")
 
-    print("... Loaded wav files. Processing now.")
+    with mp.Pool(mp.cpu_count()) as pool:
+        wav_sizes = pool.map(get_wav_file_size, samples_abs_file_paths)
 
-    bar = progressbar.progressbar(zip(abs_paths, wavs))
+    Logger.info("... Loaded wav files. Processing now.")
+    Logger.info("Checking for invalid samples...")
 
-    for file_data in bar:
-
-        path, wav = file_data
-
-        path_no_ext = path.rstrip(".wav")
-        basename_no_ext = os.path.basename(path_no_ext)
-        metadata_json_path = path_no_ext + ".json"
-
-        try:
-            true_trans = transcription_mapping[basename_no_ext]
-
-        except KeyError:
-
-            s = "\nFile not found in transcriptions file: {}".format(
-                basename_no_ext
-            )
-            s += "\nProbably invalid (only silence or not long enough)."
-            s += "\nDeleting {}".format(basename_no_ext)
-            print(s)
-            os.remove(
-                os.path.abspath(
-                    os.path.join(wav_dir, path)
-                )
-            )
-            print("... Deleted {}\n".format(basename_no_ext))
-            continue
-
+    invalid_counter, valid_counter = 0, 0
+    transcription_paths = list(transcription_mapping.keys())
+    cleaned_paths, transcriptions = [], []
+    for path in samples_abs_file_paths:
+        basename_no_ext = os.path.basename(path).rstrip(".wav")
+        if basename_no_ext not in transcription_paths:
+            invalid_counter += 1
+            Logger.warn("File not found in transcriptions file: {}".format(path))
+            Logger.warn("Probably invalid (only silence or not long enough) ==> Deleting.")
+            os.remove(path)
+            Logger.warn("... Deleted {}\n".format(path))
         else:
-            n_samples = wav.size
+            valid_counter += 1
+            cleaned_paths.append(path)
+            transcriptions.append(transcription_mapping[basename_no_ext])
 
-            json_data = [
-                {
-                    "correct_transcription": true_trans,
-                    "n_samples": int(n_samples),
-                }
-            ]
+    Logger.info("Found {} invalid examples and deleted them".format(invalid_counter))
+    Logger.info("Processing remaining {} valid files...".format(valid_counter))
 
-            with open(metadata_json_path, "w+") as f:
+    with mp.Pool(mp.cpu_count()) as pool:
+        json_data = pool.map(get_json_metadata, zip(transcriptions, wav_sizes))
+    Logger.info("Generated json metadata.")
 
-                # save disk space in json by not using *any* delimiter
-                # whitespace
-                json.dump(json_data, f, separators=(',', ':'))
+    with mp.Pool(mp.cpu_count()) as pool:
+        json_paths = pool.map(dump_json, zip(cleaned_paths, json_data))
+    Logger.info("Wrote {} json metadata files.".format(len(json_paths)))
 
-            soundfile.write(path, wav.astype(np.int16), 16000, subtype="PCM_16", format="WAV")
+    return samples_transcription_file_path, cleaned_paths, json_paths
+
+
+def create_tar_archive(major, minor, fps):
+
+    tar_fname = "{}-{}.tar.gz".format(major, minor)
+
+    def convert(x):
+        x = x.split("samples")[-1].lstrip(".").lstrip("/")
+        return os.path.join("./samples", x)
+
+    fps = l_map(convert, fps)
+
+    with ProgressBar(max_value=len(fps) + 1) as tar_bar:
+        with tarfile.open(tar_fname, "w:gz") as tar:
+            for fp in fps:
+                tar_bar.update(1)
+                tar.add(fp)
+
+    return tar_fname
+
+
+def upload_to_s3(tar_fname):
+
+    total_size = os.path.getsize(tar_fname)
+
+    s3 = boto3.client('s3')
+
+    with ProgressBar(max_value=total_size) as s3_bar:
+
+        def _upload_progress(chunk):
+            s3_bar.update(s3_bar.value + chunk)
+
+        s3.upload_file(
+            Bucket="cleverspeech-data",
+            Key=tar_fname.lstrip("./"),
+            Filename=tar_fname,
+            Callback=_upload_progress,
+        )
+
+
+def main(in_dir, major, minor):
+    transcription_file_path = os.path.join(in_dir, "test.csv")
+
+    samples_dir = make_directory_structure(major, minor)
+
+    out_transcription_file_path, out_wav_file_paths, out_json_paths = process(
+        in_dir, transcription_file_path, samples_dir
+    )
+    all_file_paths = out_wav_file_paths + out_json_paths + [out_transcription_file_path]
+
+    Logger.info("Processing complete. Creating archive from {} files...".format(
+        len(all_file_paths))
+    )
+    tar_file_name = create_tar_archive(
+        major, minor, all_file_paths
+    )
+    Logger.info("... Created archive. ")
+    Logger.info("Uploading to S3...")
+    upload_to_s3(tar_file_name)
+    Logger.info("... Uploaded. Script complete. Have a great day!")
 
 
 if __name__ == '__main__':
-    indir, transcript_file_path = sys.argv[1:]
-    main(indir, transcript_file_path)
+    main(*sys.argv[1:])
+
+
+
+
+
 
